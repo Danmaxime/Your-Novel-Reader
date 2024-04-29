380dca2cbdc3fc4fc43575e38a112aa0740d58cf
danmaxime <danielmaxime719@gmail.com>
2024-04-29 13:09:23 -0400

    finished generalization refactoring & content grabbing

--- myspider.py
+++ myspider.py
@@ -1,44 +1,77 @@
-
-import scrapy
 import os
 import re
 import sys
+import shutil
+import json
+import scrapy
+from TTS.api import TTS
+import torch
+from moviepy.editor import *
 
 class MySpider(scrapy.Spider):
     name = 'myspider'
-    start_urls = []
-    starting_chapter_number = 0
-    next_chapter_number = 0
-    ending_chapter_number = 0
-    book_title = ''
-    # Read the chapter number from the file
-    with open('chapternumber.txt', 'r') as file:
-        chapter_number = int(file.read().strip())
+    grab_count = 0
 
-    # Append the URLs to the start_urls list
-    for i in range(chapter_number, chapter_number + 2):
-        start_urls.append(f'https://lightnovelpub.vip/novel/dimensional-descent-1216/chapter-{i}')
+    @staticmethod
+    def getchaptergrabcount(path):
+        with open(f'{path}', 'r', encoding='utf-8') as config:
+            config_obj = json.load(config)
+        chapter_grab_count = int(config_obj['chapter_grab_count'])
+        return chapter_grab_count
 
-    # Update the chapter number in the file
-    with open('chapternumber.txt', 'w') as file:
-        starting_chapter_number = chapter_number
-        next_chapter_number = chapter_number + 2
-        ending_chapter_number = next_chapter_number - 1
-        file.write(str(next_chapter_number))
+    @staticmethod
+    def getconfig(path):
+        with open(f'{path}', 'r', encoding='utf-8') as config:
+            config_obj = json.load(config)
+        return config_obj
 
-    def parse(self, response):
-        self.book_title = response.css('a.booktitle::text').get()
+    def start_requests(self):
+        open_json = open('novel_list.json')
+        config_data = json.load(open_json)
+        open_json.close()
+
+        valid_novel_config_list = [config_data[novel] for novel in config_data if os.path.isfile(config_data[novel])]
+
+        for valid_config in valid_novel_config_list:
+            with open(f'{valid_config}', 'r', encoding='utf-8') as config:
+                config_obj = json.load(config)
+
+            book_title = config_obj['book_title']
+            starting_chapter_number = config_obj['next_chapter_num']
+            book_base_url = config_obj['book_base_url']
+            chapter_grab_count = self.getchaptergrabcount(valid_config)
+            next_chapter_number = starting_chapter_number + chapter_grab_count
+            for i in range(starting_chapter_number, next_chapter_number):
+                is_last_request = i == next_chapter_number - 1
+                yield scrapy.Request(f'{book_base_url}{i}', self.parse,
+                                     cb_kwargs={'config_path': valid_config, 'next_chapter_number': next_chapter_number,
+                                                'book_title': book_title,
+                                                'starting_chapter_number': starting_chapter_number,
+                                                'is_last_request': is_last_request})
+
+    def parse(self, response, config_path, next_chapter_number, book_title, starting_chapter_number, is_last_request):
+        self.grab_count = self.grab_count + 1
         chapter_title = response.css('span.chapter-title::text').get()
         paragraphs = response.css('div#chapter-container p::text').getall()
+        title_parts = chapter_title.split(":")
 
-        # Write each chapter to a new file with the chapter number and name as the file name
-        with open(f'{chapter_title}.txt', 'w') as f:
+        with open(f'{title_parts[0]}.txt', 'w', encoding='utf-8') as f:
             f.write(chapter_title + '\n\n')
             for paragraph in paragraphs:
                 f.write(paragraph + '\n\n')
 
-    def close(self, reason):
-        print("Spider closed: ", reason)
-        print("Spider closed, running sortchapters.py")
-        sys.argv = ['sortchapters.py', f'{self.book_title}', f'{self.starting_chapter_number}', f'{self.ending_chapter_number}']
-        exec(open('sortchapters.py').read())
+        needed_grab_count = int(self.getconfig(config_path)['chapter_grab_count'])
+
+        if self.grab_count == needed_grab_count:
+            self.grab_count = 0
+            with open(f'{config_path}', 'r', encoding='utf-8') as config:
+                config_obj = json.load(config)
+
+            with open(f'{config_path}', 'w', encoding='utf-8') as config:
+                config_obj['next_chapter_num'] = next_chapter_number
+                json.dump(config_obj, config, indent=4)
+
+            print("All requests for this config have been processed, running sortchapters.py")
+            sys.argv = ['./scripts/sortchapters.py', f'{book_title}', f'{starting_chapter_number}',
+                        f'{next_chapter_number}']
+            exec(open('./scripts/sortchapters.py').read())
